235bf30a17546a9ea575091323b8cdebbc3b5a00
haianh <vuhaianh1990@gmail.com>
2022-10-03 15:19:41 +0700

    Initialize project

---
+++ after/plugin/onedark.rc.lua
@@ -0,0 +1,12 @@
+-- Lua
+require('onedark').setup {
+  style = 'warmer',
+  term_colors = true,
+  highlights = {},
+  options = {
+    theme = 'onedark'
+  }
+}
+
+
+require('onedark').load()
---
+++ lua/craftzdog/base.lua
@@ -0,0 +1,54 @@
+vim.cmd("autocmd!")
+
+local opt = vim.opt
+local g = vim.g
+local cmd = vim.cmd
+local wo = vim.wo
+
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+wo.number = true
+wo.relativenumber = true
+
+opt.title = true
+opt.autoindent = true
+opt.smartindent = true
+opt.hlsearch = true
+opt.backup = false
+opt.showcmd = true
+opt.cmdheight = 1
+opt.laststatus = 2
+opt.expandtab = true
+opt.scrolloff = 10
+opt.shell = 'fish'
+opt.backupskip = { '/tmp/*', '/private/tmp/*' }
+opt.inccommand = 'split'
+opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+opt.smarttab = true
+opt.breakindent = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.wrap = false -- No Wrap lines
+opt.backspace = { 'start', 'eol', 'indent' }
+opt.path:append { '**' } -- Finding files - Search down into subfolders
+opt.wildignore:append { '*/node_modules/*' }
+opt.mouse = 'a'
+-- opt.foldmethod = 'indent'
+-- opt.foldmethod = "expr"
+-- opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Undercurl
+cmd([[let &t_Cs = "\e[4:3m"]])
+cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
+
+-- Add asterisks in block comments
+opt.formatoptions:append { 'r' }
+
